8c9bd14b4ad8baf49b456562f423f123ce75c789
Dolnapa <smile_yingying@hotmail.com>
2017-10-26 16:29:05 +0700

    first commit

---
+++ src/app/components/myfav/myfav.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-myfav',
+  templateUrl: './myfav.component.html',
+  styleUrls: ['./myfav.component.css']
+})
+export class MyfavComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
